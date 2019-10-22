3c83f14518eb0166c5892a43f39938818c3b4443
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-10-22 18:03:20 +0900

    [ADD] Start adding map markers

---
+++ frontend/components/MapMarker.vue
@@ -0,0 +1,16 @@
+<template>
+  <GmapMarker 
+    :position="{ lat: marker.lat, lng: marker.lng }"
+  />
+</template>
+
+<script>
+export default {
+  name: 'MapMarker',
+  props: {
+    marker: { type: Object }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,12 +1,54 @@
 <template>
   <div>
     <GmapMap
-      :center="{ lat: 10, lng: 10 }"
-      :zoom="7"
-      map-type-id="terrain"
-      style="width: 500px; height: 300px"
-    />
+      :center="{ lat: 37.5012, lng: 127.0396 }"
+      :zoom="17"
+      map-type-id="roadmap"
+      style="width: 100%; height: 100vh;"
+    >
+      <!-- <GmapMarker :position="position" />
+      <GmapMarker
+        v-for="m in markers"
+        :key="m.id"
+        :position="{ lat: m.lat, lng: m.lng }"
+      /> -->
+      <MapMarker v-for="m in markers" :key="m.id" :marker="m" />
+    </GmapMap>
   </div>
 </template>
 
-<script></script>
+<script>
+import MapMarker from '~/components/MapMarker.vue'
+
+export default {
+  components: {
+    MapMarker
+  },
+  data: () => ({
+    position: { lat: 37.501, lng: 127.0396 },
+    markers: [
+      {
+        id: 1,
+        lat: 37.5012,
+        lng: 127.0396,
+        name: 'marker1',
+        label: 0
+      },
+      {
+        id: 2,
+        lat: 37.5012,
+        lng: 127.042,
+        name: 'marker2',
+        label: 1
+      },
+      {
+        id: 3,
+        lat: 37.5012,
+        lng: 127.044,
+        name: 'marker3',
+        label: 0
+      }
+    ]
+  })
+}
+</script>
