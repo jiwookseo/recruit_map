80d5b3d3d17c6b20a6aaa093f69deacd12242556
Jiwook Seo <jiwookseo.dev@gmail.com>
2019-10-23 11:01:26 +0900

    [FIX] Remove unused code

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,25 +1,9 @@
 # model
 from .models import Company, Job, Station, Route
 from .serializers import CompanySerializer, JobSerializer, StationSerializer, RouteSerializer
-from django.shortcuts import get_object_or_404
 
 # rest_framework
-from rest_framework import status, renderers
-from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.decorators import api_view
-from rest_framework.reverse import reverse
 from rest_framework import viewsets
-
-
-@api_view(('GET',))
-def api_root(request, format=None):
-    return Response({
-        'companies': reverse('api:companies_list', request=request, format=format),
-        # 'job': reverse('api:job_list', request=request, format=format),
-        # 'station': reverse('api:station_list', request=request, format=format),
-        # 'routes': reverse('api:routes_list', request=request, format=format),
-    })
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
