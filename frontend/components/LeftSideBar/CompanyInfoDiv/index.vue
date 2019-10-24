16ce827915bfb8be12c74341172a1b743cefc4ca
KimDoWoo <kaei3546@naver.com>
2019-10-24 17:37:33 +0900

    [ADD]Company Detail Info Directory

---
+++ frontend/components/LeftSideBar/CompanyInfoDiv/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="company--detial-info"></section>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+.company--detial-info {
+  width: 380px;
+}
+</style>
