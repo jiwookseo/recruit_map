1ec663ae6d88fc8e13fdf5dd2a185351bc7c26d3
KimDoWoo <kaei3546@naver.com>
2019-10-25 16:23:47 +0900

    [MOD] Title

--- frontend/components/LeftSideBar/CompanyDetailInfo/Title.vue
+++ frontend/components/LeftSideBar/CompanyDetailInfo/Title.vue
@@ -3,24 +3,30 @@
     <h1>{{company.name || '이름'}}</h1>
     <p class="c-di-t-st">
       <span>{{ company.ind_name }}</span>
-      <span>신입 {{company.start_salary}}</span>
-      <span>평균 {{company.avg_salary}}</span>
     </p>
-    <p>
-      <span>회사홈페이지 :</span>
-      <a target="_blank" :href="company.href">{{company.href}}</a>
+    <hr class="c-di-t-li" />
+    <p class="c-di-t-sp">
+      신입 평균
+      <span>{{company.start_salary}} 만원</span>
     </p>
-    <p>
+    <p class="c-di-t-ap">
+      직원 평균
+      <span>{{company.avg_salary}} 만원</span>
+    </p>
+    <p class="c-di-t-cp">
+      <a target="_blank" :href="company.href">회사홈페이지 바로 가기</a>
+    </p>
+    <p class="c-di-t-rp">
       <a target="_blank" :href="company.saramin_url">
         <span>채용 공고 바로 가기</span>
       </a>
     </p>
-    <p>
+    <p class="c-di-t-jc">
       현재 진행중인 채용 공고 개수
       <span>{{company.jobs_count}}</span>
     </p>
-    <p>
-      <span>power by saram in</span>
+    <p class="saramin">
+      <span>powered by saramin</span>
     </p>
   </div>
 </template>
@@ -50,5 +56,64 @@ export default {
   h1 {
     font-weight: 500;
   }
+  a {
+    text-decoration: none;
+    color: #181818;
+  }
+  p {
+    margin-top: 15px;
+  }
+}
+
+.c-di-t-st {
+  margin-top: 4px !important;
+}
+
+.c-di-t-li {
+  margin-top: 5px;
+}
+.c-di-t-sp,
+.c-di-t-ap {
+  width: 47%;
+  display: inline-block;
+  span {
+    background-color: red;
+    padding: 5px 10px;
+    color: white;
+    border-radius: 5px;
+  }
+}
+.c-di-t-rp,
+.c-di-t-cp {
+  width: 55%;
+  background-color: #4876ef;
+  padding: 10px;
+  border-radius: 5px;
+  text-align: center;
+  a {
+    color: #fff;
+  }
+}
+
+.c-di-t-cp {
+  background-color: #181818;
+  color: #fff;
+}
+
+.c-di-t-jc {
+  span {
+    color: #181818;
+    font-size: 18px;
+    font-weight: 550;
+  }
+}
+
+.saramin {
+  width: 35%;
+  background-color: #4876ef;
+  color: #fff;
+  padding: 5px;
+  text-align: center;
+  font-size: 12px;
 }
 </style>
