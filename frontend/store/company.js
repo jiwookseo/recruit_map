39458a32c77824e6180f6783b3b36a8c38312505
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-10-28 15:19:51 +0900

    [MOD] Move station-related data into separate Vuex module

--- frontend/store/company.js
+++ frontend/store/company.js
@@ -2,17 +2,13 @@ export const state = () => ({
   hoveredCompany: {},
   selectedCompany: {},
   allCompanies: [],
-  routesFromStation: [], // transit time info from specified station
   companyDetail: {},
-  departureStationID: 961 // starting point
 })
 
 export const getters = {
   getHoveredCompany: (state) => state.hoveredCompany,
   getSelectedCompany: (state) => state.selectedCompany,
   getAllCompanies: (state) => state.allCompanies,
-  getDepartureStationID: (state) => state.departureStationID,
-  getRoutesFromStation: (state) => state.routesFromStation,
   getCompanyDetail: (state) => state.companyDetail
 }
 
@@ -26,27 +22,19 @@ export const mutations = {
   setAllCompanies: (state, payload) => {
     state.allCompanies = payload
   },
-  setDepartureStationID: (state, payload) => {
-    state.departureStationID = payload
-  },
-  setRoutesFromStation: (state, payload) => {
-    state.routesFromStation = payload
-  },
   setCompanyDetail: (state, payload) => {
     state.companyDetail = payload
   }
 }
 export const actions = {
-  setAllCompanies({ commit }, payload) {
+  setAllCompanies({
+    commit
+  }, payload) {
     commit('setAllCompanies', payload)
   },
-  setDepartureStationID({ commit }, payload) {
-    commit('setDepartureStationID', payload)
-  },
-  setRoutesFromStation({ commit }, payload) {
-    commit('setRoutesFromStation', payload)
-  },
-  setCompanyDetail({ commit }, payload) {
+  setCompanyDetail({
+    commit
+  }, payload) {
     commit('setCompanyDetail', payload)
   }
 }
