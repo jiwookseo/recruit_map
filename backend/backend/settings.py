204feec8e3d3d729a2756e1c1d0674d97f4bcfd2
Jiwook Seo <jiwookseo.dev@gmail.com>
2019-11-06 09:17:28 +0900

    [MOD] Update backend settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+NODE_ENV = os.getenv("NODE_ENV", "develop")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +25,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY",
                        "cn0d*vpk#2#-1^^*w9+x-h15e5&pzj+p8o$fbzwua4anr%@9fp")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if NODE_ENV == "production" else True
 
 ALLOWED_HOSTS = ["127.0.0.1", "52.78.29.170", "localhost"]
 
